f9b4a38b54525a2f76edea86d47afb35557f7fd7
yoonsun lee <yoonsunlee150@gmail.com>
2021-10-10 08:11:35 -0400

    Netlify Fix

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,6 @@
 <script>
-  import logo from '../../static/apple-logo.png';
 </script>
 
 <div class="bg-black w-full p-2 px-10 md:px-20 lg:px-52">
-  <img src={logo} alt="logo" class="h-6">
+<h1 class="text-3xl text-white font-bold text-center m-12">Yoonsun Lee </h1>
 </div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <Header />
-<div class="w-full min-h-screen bg-gradient-to-r from-yellow-600 via-pink-500 to-purple-500 px-10 md:px-20 lg:px-52 py-20">
+<div class="w-full min-h-screen bg-gradient-to-r from-blue-600 via-pink-500 to-green-500 px-10 md:px-20 lg:px-52 py-20">
   <slot></slot>
 </div>
