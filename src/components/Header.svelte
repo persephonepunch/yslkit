5e7c5de31e79bb3e648744be991741883dc4298c
yoonsun lee <yoonsunlee150@gmail.com>
2021-10-10 08:41:42 -0400

    new copy in header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,5 +2,5 @@
 </script>
 
 <div class="bg-black w-full p-2 px-10 md:px-20 lg:px-52">
-<h1 class="text-3xl text-white font-bold text-center m-12">Yoonsun Lee </h1>
+<h3 class="text-3xl text-white font-bold text-center m-12">This music is loaded from an API</h3>
 </div>
