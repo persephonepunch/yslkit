dd0fb97c2461e54a41038ad5a6d66c91f2f27cce
yoonsun lee <yoonsunlee150@gmail.com>
2021-10-10 05:48:30 -0400

    sample files

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
-import preprocess from "svelte-preprocess";
 /** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
     kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	},
-
-    preprocess: [preprocess({
-        postcss: true
-    })]
+        // hydrate the <div id="svelte"> element in src/app.html
+        target: '#svelte',
+        adapter: adapter()
+    }
 };
 
 export default config;
