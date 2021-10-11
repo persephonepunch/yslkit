9e41ca4ecf5f661c0773da9b7dc3b928e8e3eece
yoonsun lee <yoonsunlee150@gmail.com>
2021-10-11 09:15:13 -0400

    Image resource update

--- src/components/Ysl.svelte
+++ src/components/Ysl.svelte
@@ -4,6 +4,6 @@
 
 <div class="mt-20 flex flex-wrap content-center">
     <a href="https://www.ysl150.com/"> 
-      <img src="../../../static/yslblue.jpeg" alt="Yoonsun Lee" class="w-1-8 ">
+      <img src="https://api-svelte.netlify.app/yslblue.jpeg" alt="Yoonsun Lee" class="w-1-8 ">
     </a>
 </div>
